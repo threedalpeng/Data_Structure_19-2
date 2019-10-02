137a126ae3537a71095d6f1883f70e1880f0ea85
Kim Jeong-Won <threedalpeng@gmail.com>
2019-10-02 10:11:29 +0900

    Update hw1

--- hw1/p1.c
+++ hw1/p1.c
@@ -2,27 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX_SIZE_I 101
+#define MAX(a, b) ((a) >= (b) ? (a) : (b))
 
-char* plus_large_int(char[], char[]);
-char* multiply_large_int(char[], char[]);
+void swap_large_int(char**, char**);
+//char* plus_large_int(char[], char[]);
+//char* multiply_large_int(char[], char[]);
 
 int main(void)
 {
     // input an operator
-    char operator;
-    scanf_s("%c", &operator);
+    char opr;
+    scanf("%c", &opr);
 
     // input two large numbers
     char input_buf[MAX_SIZE_I] = {0};
-    scanf_s("%s", input_buf);
+    scanf("%s", input_buf);
     char* front = (char*)malloc(sizeof(char) * (strlen(input_buf) + 1));
     strcpy(front, input_buf);
     memset(input_buf, '\0', strlen(front));
-    scanf_s("%s", input_buf);
+    scanf("%s", input_buf);
     char* back = (char*)malloc(sizeof(char) * (strlen(input_buf) + 1));
     strcpy(back, input_buf);
 
-    switch(operator)
+    // set larger-digits number to front
+    printf("front: %s\nback : %s", front, back);
+    if (strlen(front) < strlen(back)) swap_large_int(&front, &back);
+    printf("\n\nfront: %s\nback : %s", front, back);
+
+    /*
+    switch(opr)
     {
     case '+':
         printf("%s", plus_large_int(front, back));
@@ -31,8 +39,15 @@ int main(void)
         printf("%s", multiply_large_int(front, back));
         break;
     }
-
+    */
     return 0;
+}
+
+void swap_large_int(char** s1, char** s2)
+{
+    char* temp = *s1;
+    *s1 = *s2;
+    *s2 = temp;
 }
 
 char* plus_large_int(char f[], char b[])
@@ -40,9 +55,9 @@ char* plus_large_int(char f[], char b[])
     int carry = 0;
     int f_len = strlen(f);
     int b_len = strlen(b);
-    char* result = (char*)malloc(sizeof(char) * (max(f_len, b_len) + 1));
+    char* result = (char*)malloc(sizeof(char) * ( MAX(f_len, b_len) + 2));
     while(f_len > 0 && b_len > 0)
     {
-        
     }
+    return result;
 }
---
+++ hw1/p2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct _Brick
+{
+    int fill, c, r;
+} Brick;
+
+
+int main(void)
+{
+    int C, R;
+    scanf("%d %d", &C, &R);
+}
